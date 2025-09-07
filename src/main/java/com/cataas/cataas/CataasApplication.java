0245a988bc81388ffe13a1409d00766ce59f715e
Moaid <moaidnasser19@gmail.com>
2025-09-06 19:13:39 -0700

    Initial commit. Generate the project.

---
+++ src/main/java/com/cataas/cataas/CataasApplication.java
@@ -0,0 +1,13 @@
+package com.cataas.cataas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CataasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CataasApplication.class, args);
+	}
+
+}
