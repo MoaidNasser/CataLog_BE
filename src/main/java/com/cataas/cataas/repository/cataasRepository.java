f73315728591c966187bb7f2b8a1d0e8c3832357
Moaid <moaidnasser19@gmail.com>
2025-09-06 23:55:58 -0700

    Implement method in service class to fetch cats from api and save them in database. And implemnt a method to get cats from database.

--- src/main/java/com/cataas/cataas/repository/cataasRepository.java
+++ src/main/java/com/cataas/cataas/repository/cataasRepository.java
@@ -1,4 +1,9 @@
 package com.cataas.cataas.repository;
 
-public class cataasRepository {
+import com.cataas.cataas.model.Cat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface cataasRepository extends JpaRepository<Cat, String> {
 }
--- src/main/java/com/cataas/cataas/service/cataasService.java
+++ src/main/java/com/cataas/cataas/service/cataasService.java
@@ -1,4 +1,37 @@
 package com.cataas.cataas.service;
 
+import com.cataas.cataas.model.Cat;
+import com.cataas.cataas.repository.cataasRepository;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+@Service
 public class cataasService {
+
+    private final cataasRepository cataasrepository;
+
+    public cataasService(cataasRepository cataasrepository) {
+        this.cataasrepository = cataasrepository;
+    }
+
+    private final WebClient client = WebClient.create("https://cataas.com");
+
+    public void getCatsFromApi() {
+        saveToDB(client.get()
+                .uri("/api/cats?skip=0&limit=5000")
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<Cat>>() {})
+                .block());
+    }
+
+    public void saveToDB(List<Cat> cats) {
+        cataasrepository.saveAll(cats);
+    }
+
+    public List<Cat> getCats() {
+        return cataasrepository.findAll();
+    }
 }
